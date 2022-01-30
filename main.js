8966b6f211a477813a2fb16b70989ebea783a7a7
Mykhtor <muxtordev@gmail.com>
2022-01-30 15:03:52 +0500

    Javascript intro

--- main.js
+++ main.js
@@ -1,2 +1,16 @@
+// alert(`siz kiritgan son ` + prompt('son kiriting: '))
+// alert(1.13 + ' va ' + 49)
+// alert(7016 + '   ' + 100)
+// alert(1 + ' ' + 2 + ' ' + 3 + ' ' + 4)
+// alert(` 5  \n 10  \n 21 `)
+// alert(`Siz kiritgan sonlar ` + '\n' + prompt('son kiriting: ') + '\n' + prompt('son kiriting: ') + '\n' + prompt('son kiriting: ') + '\n' + prompt('son kiriting: '))
+// alert(`Siz kiritgan ma'lumotlar: ` + prompt('son kiriting: ') + ' ' + prompt('son kiriting: ') + '\n' + prompt('son kiriting: ' ) + ' sm')
+// alert(`14 sonini 5-darajasi: ` + '\n' + Math.pow(14,5))
 
-alert(prompt )
+// let x = prompt('son kiriting:')
+// alert( 'Siz kiritgan ' + x + ' ning' + '\n' + '4-darajasi ' + Math.pow(x, 4) + '\n' + '6-darajasi ' + Math.pow(x, 6))
+// let a = 10
+// let b = 4
+// let x = ((a-b)*2)
+// let n = prompt('Son kiriting: ')
+// alert(('(10-4)*2 ning ' + n + 'chi darajasi ' + Math.pow(x, n)))
